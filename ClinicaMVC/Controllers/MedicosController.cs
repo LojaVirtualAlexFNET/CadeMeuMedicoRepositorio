71e2006622c65b096e3dec679f7b34f2330b3506
alexnf <alexnfnet@hotmail.com>
2016-07-06 11:36:39 -0300

    Criando o Controle Adicioanar e Editar

--- ClinicaMVC/Controllers/MedicosController.cs
+++ ClinicaMVC/Controllers/MedicosController.cs
@@ -1,6 +1,7 @@
 ﻿using ClinicaMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,50 @@ namespace ClinicaMVC.Controllers
         {
             var medicos = banco.Medicos.Include("Cidade").Include("Especialidade");
             return View(medicos);
+        }
+        public ActionResult Adicionar()
+        {
+            ViewBag.IdCidade = new SelectList(banco.Cidades, "IDCidade", "Nome");
+            ViewBag.IdEspecialidade = new SelectList(banco.Especialidades, "IDEspecialidade", "Nome");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Adicionar(Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                banco.Medicos.Add(medico);
+                banco.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCidade = new SelectList(banco.Cidades, "IDCidade", "Nome", medico.IDCidade);
+            ViewBag.IdEspecialidade = new SelectList(banco.Especialidades, "IDEspecialidade", "Nome", medico.IDEspecialidade);
+            return View(medico);
+        }
+        public ActionResult Editar(long id)
+        {
+            Medico medico = banco.Medicos.Find(id);
+            ViewBag.IdCidade = new SelectList(banco.Cidades, "IdCidade", "Nome", medico.IDCidade);
+            ViewBag.IdEspecialidade = new SelectList(banco.Especialidades, "IDEspecialidade", "Nome",medico.IDEspecialidade);
+            return View(medico);
+        }
+        [HttpPost]
+        public ActionResult Editar(Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                banco.Entry(medico).State = EntityState.Modified;
+                banco.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDCidade = new SelectList(banco.Cidades, "IDCidade", "Nome", medico.IDCidade);
+            ViewBag.IDEspecialidade = new SelectList(banco.Especialidades, "IDEspecialidade", "Nome", medico.IDEspecialidade);
+            return View(medico);
+        }
+        public ActionResult Editar(int id)
+        {
+
+            return View();
         }
     }
 }
